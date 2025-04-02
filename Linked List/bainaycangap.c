0de72a0507ec7a74b7e1a00a416d07627a115fdb
NeroxLucas <neroxvxi2005@gmail.com>
2025-04-02 16:40:52 +0700

    /...

---
+++ Linked List/bainaycangap.c
@@ -0,0 +1,17 @@
+/*
+given an array as follows:
+X = [10, 30, 4, 51, 13, 8, 90]
+binary search tree is used to store the array numbers. draw the tree structure for this array :
+note: the following format can be used to design your tree in the text file, where you can replace A, B, ... G with the array numbers 
+                                                A
+                                B                           C
+                    D                   E               F       G
+ 
+
+once we have obtained the BST for X, we want to visit all node in this tree. if we use the following algo, show the reuslt
+    + Recursively visit the right child node
+    + Recursively visit the left child node
+    + visit a node and display its value 
+
+DO NOT IMPLEMENT THIS CODE IN C
+*/
