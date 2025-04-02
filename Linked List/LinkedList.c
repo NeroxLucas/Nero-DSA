9e9a4cc79ce5075d8298976c70972d35bb7be7e5
NeroxLucas <neroxvxi2005@gmail.com>
2025-04-02 16:14:32 +0700

    ....

--- Linked List/LinkedList.c
+++ Linked List/LinkedList.c
@@ -1,3 +1,16 @@
-#include <stdio.h>
+//suppose an array of size >=6. its values must be initialized by inters of ur choice. we want to arrange this array and the output array so that the prime number are first and then non-prime numbers.
+//note: prime numbers are natural numbers that are divisible by 1 and itself
+//write a function to check a number is prime
+//a function to swap prime's position in the array
+//a function to display an input array
+//main function to test 
 
-//write a program using recursion to find maximum of a number in a array 
+
+
+//bai 2 
+//implement a clock data structure to manipulate time
+//a structure to store hours (values [0;23]), minutes [0;59], seconds [0;59];
+//a function to initialize a clock variable with 3 values (hour, minutes, second)
+//a function to increase a clock variable by 1 second
+//a function to caluclate the difference between 2 clock variables 
+//a main function to test 
